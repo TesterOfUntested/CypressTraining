5f7253ef4fe79ce0006868445e3a4b373b1de422
Krzysztof Bigos <sprawdzany96@gmail.com>
2021-10-31 11:47:48 +0100

    Made first POM with beforeTest

--- cypress/integration/Demoblaze/main.spec.js
+++ cypress/integration/Demoblaze/main.spec.js
@@ -1,10 +1,13 @@
+import mainPage from "./POM/mainPage";
+
 describe('User authentication tests', () => {
     beforeEach(() => {
         cy.visit('www.demoblaze.com/');
+        cy.title().should('eq', mainPage.pageTitle)
       })
 
       it('Test sign up', () => {
-        cy.title().should('eq', 'STORE');
+        
       })
 
 })
