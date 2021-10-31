4dfb38a51bcdf8c71584005e5a53682272ad189a
Krzysztof Bigos <sprawdzany96@gmail.com>
2021-10-31 21:34:10 +0100

    Finished signUp script with username generator

--- cypress/integration/Demoblaze/main.spec.js
+++ cypress/integration/Demoblaze/main.spec.js
@@ -1,15 +1,21 @@
 import mainPage from "./POM/mainPage";
+import signUpPage from "./POM/signUpPage";
 import upperMenu from "./POM/upperMenu";
+import generator from "./utils/generator";
 
 describe('User authentication tests', () => {
-    beforeEach(() => {
+  
+  var username = generator.generateUsername()
+
+    before(() => {
         cy.visit('www.demoblaze.com/');
         cy.title().should('eq', mainPage.pageTitle)
+        
       })
 
       it('Test sign up', () => {
-        upperMenu.goToHome()
-        //cy.get('#login2').click()
+        upperMenu.goToSignUp()
+        signUpPage.signUp(username, 'password')
       })
 
 })
