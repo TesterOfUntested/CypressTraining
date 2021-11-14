aa257f5a281888ac2e30f4d18e766be1d67f99a1
Krzysztof Bigos <sprawdzany96@gmail.com>
2021-11-14 11:43:09 +0100

    Changed generated username

--- cypress/integration/Demoblaze/utils/generator.js
+++ cypress/integration/Demoblaze/utils/generator.js
@@ -1,10 +1,10 @@
-class utils{
+class generator{
 
     generateUsername(){
-        var base_username = "TesterOfUntested"
+        var base_username = "TesterOfUntested1"
         var sufix_username = Date.now()
         return base_username+sufix_username
     }
 
 }
-export default new utils
+export default new generator
