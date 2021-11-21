2fa886747ad7cc9e8b35654a3b591205761d7cf9
Krzysztof Bigos <sprawdzany96@gmail.com>
2021-11-21 10:50:37 +0100

    Cart laptops tests

--- cypress/integration/Demoblaze/main.spec.js
+++ cypress/integration/Demoblaze/main.spec.js
@@ -5,45 +5,55 @@ import upperMenu from "./POM/upperMenu";
 import phonesPage from "./POM/phonesPage";
 import generator from "./utils/generator";
 import phonePage from "./POM/phonePage";
+import laptopsPage from "./POM/laptopsPage";
+import laptopPage from "./POM/laptopPage";
 
 describe('User authentication tests', () => {
   
-  var username = generator.generateUsername()
-  var phoneName = 'Samsung galaxy s6'
+  var username = generator.generateUsername();
+  var phoneName = 'Samsung galaxy s6';
+  var laptopName = 'Sony vaio i7';
 
-    before(() => {
+      beforeEach(() => {
         cy.visit('www.demoblaze.com/');
-      })
+      });
 
       it('Main Page not logged tests', () => {
-        mainPage.notLoggedChecker()
-      })
+        mainPage.notLoggedChecker();
+      });
 
       it('Test sign up', () => {
-        upperMenu.goToSignUp()
-        signUpPage.signUp(username, 'password')
-      })
+        upperMenu.goToSignUp();
+        signUpPage.signUp(username, 'password');
+      });
 
       it('Test log in', () => {
-        upperMenu.goToLogIn()
-        logInPage.logIn(username, 'password')
-      })
+        upperMenu.goToLogIn();
+        logInPage.logIn(username, 'password');
+      });
 
       it('Main Page logged tests', () => {
-        mainPage.loggedChecker()
-      })
+        mainPage.loggedChecker();
+      });
 
       it('Phones cart test', () =>{
-        mainPage.goToPhones()
-        phonesPage.checkHowManyPhones(7)
-        phonesPage.goToPhonePage(phoneName)
+        mainPage.goToPhones();
+        phonesPage.checkHowManyPhones(7);
+        phonesPage.goToPhonePage(phoneName);
         phonePage.addPhoneToCart(phoneName)
-      })
+      });
+
+      it('Laptop cart test', () => {
+        mainPage.goToLaptops();
+        laptopsPage.checkHowManyLaptops(6);
+        laptopsPage.goToLaptopPage(laptopName);
+        laptopPage.addLaptopToCart(laptopName);
+      });
 
     after(() => {
-      upperMenu.goToHome()
-      if(upperMenu.checkIfLogged())
-        upperMenu.goToLogOut()
-    })
+      upperMenu.goToHome();
+      if(upperMenu.checkIfLogged());
+        upperMenu.goToLogOut();
+    });
 
 })
