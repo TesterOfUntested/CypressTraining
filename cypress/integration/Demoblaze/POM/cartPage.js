5902c95e908ca5ee418912e92a0442ee65ec9de7
Krzysztof Bigos <sprawdzany96@gmail.com>
2021-11-21 15:57:02 +0100

    cart checking added

--- cypress/integration/Demoblaze/POM/cartPage.js
+++ cypress/integration/Demoblaze/POM/cartPage.js
@@ -2,7 +2,16 @@ class cartPage {
 
     pageTitle = "STORE"
     get totalAmount () { return $('#totalp') }
-    get tableProducts () { return $('.table .table-bordered .table-hover .table-striped')}
+    get tableProducts () { return $('.table table-bordered table-hover table-striped')}
+
+    checkTotalAmount(totalAmount){
+        cy.get("#totalp").should('eq', totalAmount)
+    }
+
+    checkIfProductAdded(productName, productPrice){
+        cy.get("td").contains(productName).should('be.visible')
+        cy.get("td").contains(productPrice).should('be.viisble')
+    }
 
 }
 export default new cartPage()
